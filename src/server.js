fe1e97173bb1443914130e742c8b628cabd3ffae
Dmitry Kostin <etharner@gmail.com>
2019-07-16 19:09:03 +1000

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const PORT = 6666;
+const HOST = '0.0.0.0';
+const API_KEY = '2e7780fe98e0f2f372985d50cfd3c847673d973182e86c5e32db66596d1c770e';
+let prices = {};
+
+const getPrices = async () => {
+  prices = await fetch(
+    `https://min-api.cryptocompare.com/data/price?fsym=AE&tsyms=USD,JPY,EUR&api_key=${API_KEY}`
+  ).then(r => r.json());
+};
+
+setInterval(getPrices, 600000);
+getPrices();
+
+const app = express();
+app.get('/price', (req, res) => {
+  res.send(prices);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
